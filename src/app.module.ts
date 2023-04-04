01f8c3b735cf73d7922f7d5395e4881b4041027e
Ruslan <rusishss@mail.ru>
2023-04-04 13:00:14 +0600

    tried CRUD operations in Nest using blogs as an example. (controller only)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogsController } from './blogs/blogs.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BlogsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/blogs/blogs.controller.ts
@@ -0,0 +1,53 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+const blogs = [
+  {
+    id: '698dcb38-5e7b-431f-a4c3-01454a994000',
+    name: 'test blog 1',
+    description: 'testing for id',
+    websiteUrl: 'https://websiteUrlgoogle.com',
+  },
+  {
+    id: '698dcb38-5e7b-431f-a4c3-01454a994001',
+    name: 'test blog 2',
+    description: 'testing for name',
+    websiteUrl: 'https://websiteUrlgoogle.com',
+  },
+  {
+    id: '698dcb38-5e7b-431f-a4c3-01454a994002',
+    name: 'test blog 2',
+    description: 'testing for some',
+    websiteUrl: 'https://websiteUrlgoogle.com',
+  },
+];
+@Controller('blogs')
+export class BlogsController {
+  @Post()
+  createBlogs(@Body() inputModel: CreateBlogInputModelType) {
+    const newBlog = {
+      id: uuid(),
+      name: inputModel.name,
+      description: inputModel.description,
+      websiteUrl: inputModel.websiteUrl,
+    };
+    blogs.push(newBlog);
+    return newBlog;
+  }
+
+  @Get()
+  getAllBlogs() {
+    return blogs;
+  }
+
+  @Get(':id')
+  getBlogById(@Param('id') blogId: string) {
+    const blog = blogs.find((b) => b.id === blogId);
+    return blog;
+  }
+}
+
+export type CreateBlogInputModelType = {
+  name: string;
+  description: string;
+  websiteUrl: string;
+};
