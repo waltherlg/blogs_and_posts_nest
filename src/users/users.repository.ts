768684f27a7183471193db647a9cabc8f66fd268
Ruslan <rusishss@mail.ru>
2023-04-15 20:09:27 +0600

    create methods for users in usersRepository: save user, create user, delete user, getUserDBType, delete all Users

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { User, UserDocument } from './users.types';
+import { PostDocument } from '../posts/posts.types';
 
 @Injectable()
-export class UsersRepository {}
+export class UsersRepository {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async saveUser(user: UserDocument) {
+    const result = await user.save();
+    return !!result;
+  }
+
+  async createUser(userDTO): Promise<string> {
+    const newUser = new this.userModel(userDTO);
+    await newUser.save();
+    return newUser._id.toString();
+  }
+
+  async deleteUserById(userId: string): Promise<boolean> {
+    if (!Types.ObjectId.isValid(userId)) {
+      return false;
+    }
+    return this.userModel.findByIdAndDelete(userId);
+  }
+
+  async getUserDBTypeById(userId): Promise<UserDocument | null> {
+    if (!Types.ObjectId.isValid(userId)) {
+      return null;
+    }
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+
+  async deleteAllUser() {
+    await this.userModel.deleteMany({});
+  }
+}
