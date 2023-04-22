dfb22e8b16cb6f356f4da374713ad6db555672a2
Ruslan <rusishss@mail.ru>
2023-04-22 15:14:13 +0600

    tried to set up an email service, made an email manager.  made a factory DTO, configured the creation of a user DTO 2

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,32 +1,16 @@
 import { UsersRepository } from './users.repository';
 import { Injectable } from '@nestjs/common';
-import { UserDBType } from './users.types';
 import { BcryptService } from '../other.services/bcrypt.service';
-import { Types } from 'mongoose';
+import { DTOFactory } from '../helpers/usersDTOfactory';
 @Injectable()
 export class UsersService {
   constructor(
     private readonly usersRepository: UsersRepository,
     private readonly bcryptService: BcryptService,
+    private readonly dtoFactory: DTOFactory,
   ) {}
   async createUser(userCreateInputModel) {
-    const passwordHash = await this.bcryptService.hashPassword(
-      userCreateInputModel.password,
-    );
-    const userDTO = new UserDBType(
-      new Types.ObjectId(),
-      userCreateInputModel.login,
-      passwordHash,
-      userCreateInputModel.email,
-      new Date().toISOString(),
-      null,
-      null,
-      true,
-      null,
-      null,
-      [],
-      [],
-    );
+    const userDTO = await this.dtoFactory.createUserDTO(userCreateInputModel);
     const newUsersId = await this.usersRepository.createUser(userDTO);
     return newUsersId;
   }
