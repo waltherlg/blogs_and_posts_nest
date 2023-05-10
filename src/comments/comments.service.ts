55b47d31c04f73a88964f0a026423507f301a22a
Ruslan <rusishss@mail.ru>
2023-05-10 11:33:16 +0600

    add CommentService, add method create comment (DTO) in this service

---
+++ src/comments/comments.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CommentsRepository } from './comments.repository';
+import { UsersRepository } from '../users/users.repository';
+import { Types } from 'mongoose';
+import { CommentDBType } from './comments.types';
+
+@Injectable()
+export class CommentsService {
+  constructor(
+    private readonly commentsRepository: CommentsRepository,
+    private readonly usersRepository: UsersRepository,
+  ) {}
+  async createComment(
+    postId: string,
+    content: string,
+    userId: string,
+  ): Promise<string> {
+    const user = await this.usersRepository.getUserDBTypeById(userId);
+    const CommentDTO = new CommentDBType(
+      new Types.ObjectId(),
+      'post',
+      postId,
+      content,
+      userId!,
+      user!.login,
+      new Date().toISOString(),
+      0,
+      0,
+      'None',
+    );
+    const createdCommentId = await this.commentsRepository.createComment(
+      CommentDTO,
+    );
+    return createdCommentId;
+  }
+}
