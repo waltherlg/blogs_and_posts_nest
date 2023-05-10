751de6be2afeeb3c6038802f9f3c989a7e0ae63d
Ruslan <rusishss@mail.ru>
2023-05-10 10:13:24 +0600

    made a mongus scheme for the comments. set up a method for preparing comments to send to the client

--- src/comments/comments.types.ts
+++ src/comments/comments.types.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument, Types } from 'mongoose';
+
 export class CommentDBType {
   constructor(
     public _id: Types.ObjectId,
@@ -30,4 +31,50 @@ export type CommentTypeOutput = {
   commentatorInfo: CommentatorInfoType;
   createdAt: string;
   likesInfo: LikesInfoType;
+};
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+export class Comment {
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId })
+  _id: Types.ObjectId;
+  @Prop()
+  parentType: string;
+  @Prop()
+  parentId: string;
+  @Prop()
+  content: string;
+  @Prop()
+  userId: string;
+  @Prop()
+  userLogin: string;
+  @Prop()
+  createdAt: string;
+  @Prop()
+  likesCount: number;
+  @Prop()
+  dislikesCount: number;
+  @Prop()
+  myStatus: string;
+  prepareCommentForOutput() {
+    return {
+      id: this._id.toString(),
+      content: this.content,
+      commentatorInfo: {
+        userId: this.userId,
+        userLogin: this.userLogin,
+      },
+      createdAt: this.createdAt,
+      likesInfo: {
+        likesCount: this.likesCount,
+        dislikesCount: this.dislikesCount,
+        myStatus: this.myStatus,
+      },
+    };
+  }
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
+CommentSchema.methods = {
+  prepareCommentForOutput: Comment.prototype.prepareCommentForOutput,
 };
